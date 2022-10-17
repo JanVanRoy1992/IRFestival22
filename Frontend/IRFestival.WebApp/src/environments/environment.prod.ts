b3bc1ccf8f107263c5095c8e5c178a23f93339e3
Jan Van Roy <Jan.VanRoy@inetum-realdolmen.world>
2022-10-17 16:24:00 +0200

    back to old api address

--- Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
+++ Frontend/IRFestival.WebApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   festivalName: "IRFestival",
-  apiBaseUrl: "https://irfestivalapimjvr.azure-api.net/v1/api/"
+  apiBaseUrl: "https://irfestivalapi.azurewebsites.net/api/"
 };

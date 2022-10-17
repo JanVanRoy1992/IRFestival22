bb485c347e393237344ac44f308bc5ee0fda11c1
Jan Van Roy <Jan.VanRoy@inetum-realdolmen.world>
2022-10-17 13:14:55 +0200

    new api url

--- Frontend/IRFestival.WebApp/src/app/api-services/appsettings-api.service.ts
+++ Frontend/IRFestival.WebApp/src/app/api-services/appsettings-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Schedule } from '../api/models/schedule.model';
 import { environment } from 'src/environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AppSettings } from '../api/models/appsettings.model';
 
@@ -14,6 +14,7 @@ export class AppSettingsApiService {
   constructor(private httpClient: HttpClient) { }
 
   getSettings(): Observable<AppSettings> {
-    return this.httpClient.get<AppSettings>(this.baseUrl);
+    const headers = new HttpHeaders().set('Ocp-Apim-Subscription-Key', '1194c998769c41a1bd03ed94f9766e1a');
+    return this.httpClient.get<AppSettings>(this.baseUrl, {'headers': headers});
   }
 }
